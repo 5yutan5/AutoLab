af1a8a153a282dbc14c8ee9c32a7473a9c8f1a56
yakitori <4yutan4@gmail.com>
2021-02-11 15:24:31 +0900

    Write details

--- setup.py
+++ setup.py
@@ -1,1 +1,24 @@
-# from setuptools import setup
+import setuptools
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    LONG_DESCRIPTION = f.read()
+
+setup(
+    name="AutoLab",
+    version="0.1.0",
+    author="Yakitori",
+    author_email="4yutan4@gmail.com",
+    description="This module help creating GUI application useing for experiments.",
+    longdescription=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/5yutan5/AutoLab",
+    install_requires=["pyqt", "psutil"],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+)
